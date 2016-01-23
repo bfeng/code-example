42f8c9da3fdf476067137b614f0eae580092df92
Bo Feng <bfeng1216@gmail.com>
2016-01-22 22:07:57 -0600

    new test for 4sum

--- src/test/java/io/github/bfeng/leetcode/FourSumTest.java
+++ src/test/java/io/github/bfeng/leetcode/FourSumTest.java
@@ -33,4 +33,14 @@ public class FourSumTest {
         List<List<Integer>> result = solution.fourSum(input, target);
         assertTrue(Utils.equalsWithoutOrder(expected, result));
     }
+
+    @Test(timeout = 10)
+    public void testFourSumLargeInput() throws Exception {
+        int[] input = new int[]{-496, -487, -446, -421, -420, -411, -409, -400, -374, -353, -322, -317, -293, -290,
+                -289, -279, -278, -260, -258, -257, -253, -247, -226, -198, -192, -182, -178, -174, -170, -163, -150,
+                -145, -140, -115, -69, -58, -50, -40, -21, -16, -13, 5, 5, 39, 39, 43, 45, 83, 83, 94, 106, 107, 117,
+                143, 166, 174, 178, 185, 185, 210, 228, 231, 239, 246, 247, 262, 300, 338, 340, 342, 344, 352, 353,
+                359, 362, 363, 372, 375, 395, 400, 412, 414, 420, 429, 430, 431, 440, 441, 486};
+        solution.fourSum(input, -2468);
+    }
 }
