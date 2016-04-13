eb8587a306509b78711a0d862f083bb257514e7d
Bo Feng <bfeng1216@gmail.com>
2016-04-12 22:19:55 -0400

    added impl and large input test

--- src/test/java/io/github/bfeng/leetcode/MedianOfTwoSortedArraysTest.java
+++ src/test/java/io/github/bfeng/leetcode/MedianOfTwoSortedArraysTest.java
@@ -18,9 +18,23 @@ public class MedianOfTwoSortedArraysTest {
     }
 
     @Test
-    public void testFindMedianSortedArrays() throws Exception {
+    public void testFindMedianSortedArraysCase1() throws Exception {
         int[] input1 = new int[]{0, 2, 4};
         int[] input2 = new int[]{1, 3};
         assertEquals(2, solution.findMedianSortedArrays(input1, input2), 1E-9);
+    }
+
+    @Test
+    public void testFindMedianSortedArraysCase2() throws Exception {
+        int[] input1 = new int[]{0, 1, 2};
+        int[] input2 = new int[]{2, 3, 4};
+        assertEquals(2, solution.findMedianSortedArrays(input1, input2), 1E-9);
+    }
+
+    @Test
+    public void testFindMedianSortedArraysCase3() throws Exception {
+        int[] input1 = new int[]{};
+        int[] input2 = new int[]{1};
+        assertEquals(1, solution.findMedianSortedArrays(input1, input2), 1E-9);
     }
 }
